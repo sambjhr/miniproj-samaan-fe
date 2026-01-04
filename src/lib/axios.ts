9f580331714171049d086cb75cdcd4fd9f7343b2
Samuel B <sambjhr.work@gmail.com>
2026-01-04 11:51:10 +0700

    fitur event (#3)

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "https://neatmarble-us.backendless.app",
+  baseURL: "http://localhost:8000",
   headers: {
     "Content-Type": "application/json",
   },
